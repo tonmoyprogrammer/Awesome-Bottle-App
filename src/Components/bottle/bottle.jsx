9768ea935a4054b98433e3b7edf13ef9ea298ae8
tonmoyprogrammer <debtonmoy583@gmail.com>
2025-12-26 00:42:31 +0600

    This adding cart commiit

--- src/Components/bottle/bottle.jsx
+++ src/Components/bottle/bottle.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './bottle.css'
 
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle,addHandleCart }) => {
     const {name,seller,price,stock,img} = bottle;
 
     return (
+
         <div className="bottle">
             <img src={img} alt="" />
             <p>Name : {name}</p>
             <p>Brand : {seller}</p>
             <p>Price : {price}</p>
             <p>Stock : {stock}</p>
-            <button>Add to cart</button>
+            <button onClick={()=>addHandleCart(bottle)}>Add to cart</button>
             
         </div>
     );
--- src/Components/bottles/bottles.jsx
+++ src/Components/bottles/bottles.jsx
@@ -1,22 +1,33 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Bottle from '../bottle/bottle';
 import './bottles.css'
 
 const Bottles = ({bottles}) => {
 
     const getBottles = use(bottles);
+    const [cart,addCart] = useState([]);
+    const addHandleCart = (bottle)=>
+    {
+      const newCart =[...cart,bottle];
+      addCart(newCart)
+    }
 
     return (
         <div>
             <h2>Beautiful water bottle</h2>
             <h3>Total Bottles :{getBottles.length} </h3>
-            <h4>add your bottles</h4>
-            <div className=""></div>
+            <h4>add your bottles : {cart.length}</h4>
+            <div className="">
+            
+
+            </div>
 
 
             <div className="bottles">
                 {
-                    getBottles.map(bottle=><Bottle key={bottle.id}bottle={bottle}></Bottle>)
+                    getBottles.map(bottle=><Bottle key={bottle.id}bottle={bottle}
+                    addHandleCart ={addHandleCart }
+                    ></Bottle>)
                 }
             </div>
             
